770e3fa76697de12ac2e50adf19c3e5437ad9875
Iman Kurniawan <imankurniawan@gmail.com>
2022-07-12 18:13:39 +0700

    test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.decorators.http import require_http_methods
 # Create your views here.
+@require_http_methods(["GET"])
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
